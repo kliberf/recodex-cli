00ac7405afd1e30e822013c0c2c1fa21d896396f
Martin Krulis <krulis@ksi.mff.cuni.cz>
2018-07-30 16:02:02 +0200

    Adding a function for registering new users.

--- recodex/plugins/users/cli.py
+++ recodex/plugins/users/cli.py
@@ -50,10 +50,42 @@ def search(api: ApiClient, context: UserContext, search_string, as_csv):
 
 
 @cli.command()
+@click.argument("email")
+@click.argument("first_name")
+@click.argument("last_name")
+@click.option('--password', help='Password. If no password is given, it is prompted.',
+              prompt=True, hide_input=True, confirmation_prompt=True)
+@click.option('--instance_id', help='Instance where the new user belongs to. If no instance is provided, instance of logged user is taken.')
+@click.option('--join_group', multiple=True, help='Id of a group which is immediately joined by the registered user. This option may be repeated.')
+@pass_user_context
+@pass_api_client
+def register(api: ApiClient, context: UserContext, email, first_name, last_name, password, instance_id, join_group):
+    """
+    Register new user with local account
+    """
+    if instance_id is None:
+        instances_ids = api.get_user(context.user_id)["privateData"]["instancesIds"]
+        if len(instances_ids) != 1:
+            click.echo("Instance ID is ambiguous. Provide explicit ID via --instance_id option.")
+            return
+        instance_id = instances_ids[0]
+
+    res = api.register_user(instance_id, email, first_name, last_name, password)
+    user_id = res['user']['id']
+    click.echo("User {id} ({first_name} {last_name}, {email}) registered in instance {instance_id}".format(
+        id=user_id, first_name=first_name, last_name=last_name, email=email, instance_id=instance_id))
+
+    for group_id in join_group:
+        api.group_add_student(group_id, user_id)
+        click.echo("User {} joined group {}".format(user_id, group_id))
+
+
+@cli.command()
 @click.argument("id")
+@click.option("--name", nargs=2, help="New name as two arguments (first_name last_name).")
 @click.option("--gravatar/--no-gravatar")
 @pass_api_client
-def edit(api: ApiClient, id, gravatar):
+def edit(api: ApiClient, id, name, gravatar):
     """
     Edit profile of a user
     """
@@ -65,6 +97,11 @@ def edit(api: ApiClient, id, gravatar):
         "email": user["privateData"]["email"],
         "gravatarUrlEnabled": user['avatarUrl'] is not None,
     }
+
+    if name is not None:
+        data["firstName"] = name[0]
+        data["lastName"] = name[1]
+
     if gravatar is not None:
         data["gravatarUrlEnabled"] = gravatar
     api.update_user(id, data)
